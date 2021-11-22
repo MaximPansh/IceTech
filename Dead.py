45efc409bd254d01a936626085ac6fa8e833b479
Kirill Blinov <blinov.kirill@yandex.ru>
2021-11-22 20:05:47 +0300

    Some Changes

--- Dead.py
+++ Dead.py
@@ -9,9 +9,10 @@ import numpy as np
 from scipy.integrate import quad
 from matplotlib import pyplot as plt
 def calc(v, h):
-    
-   # r, f_fur, yj - вопросы
-    
+    """
+    !!!TO DO!!!
+   r, f_fur, yj - вопросы
+    """
     E = 6*10**9  #  модуль упругости
    # h = 2.5  #  толщина льда
     u = 0.33 # к-т Пуассона
@@ -27,6 +28,10 @@ def calc(v, h):
     yj= 0.126 # декремент затухания колебаний
     D1 = D/(po*g) 
     X1 = po_l *h/(po*g) 
+    X0 = 0 # снег
+    X = (po_l*h)/po
+    n = 0.14
+    n1 = n /(po*g)
     a = -h*po_l/po #глубина погружения льдины при статическом равновесии
     
     ## НЕ ПРАВИЛЬНО ПОСЧИТАНО ВОЛНОВОЕ ЧИСЛО
@@ -42,19 +47,24 @@ def calc(v, h):
     
     
     
-    def interate_J (R, O, yj, v, tay):
-        
+    def interate_J (R, O, v, tay):
+        global yj
+        G = (r*(g*(a+H))**0.5)/(1+X1*r**(2)*g*(a+H))
+        yj = (n1*(r**4)*g*math.tanh(r)*H)/(8*G*(1+(X+X0)*r*math.tanh(r)*H))
         d1 = r*v*math.cos(O) + -1*tay                     
         d2 = r*v*math.cos(O) + pow(-1,2)*tay
         return 2/(d1*d2)*2.7182**(1j*r*R*math.cos(O-yj))
     
     
     
-    J = quad(interate_J, -math.pi/2, 3*math.pi/2 , args=(R, yj, v, tay))
+    J = quad(interate_J, -math.pi/2, 3*math.pi/2 , args=(R, v, tay))
     
     
     def integrate_W(r,H):
+        global yj
         G = (r*(g*(a+H))**0.5)/(1+X1*r**(2)*g*(a+H)) #частота волнения для мелкой воды
+        yj = (n1*(r**4)*g*math.tanh(r)*H)/(8*G*(1+(X+X0)*r*math.tanh(r)*H))
+        
         f_fur = (2*3.14)**0.5*yj    # фурье образ функции декардовых координат ОЧЕНЬ СПОРНЫЙ ВОПРОС
         f_fur = 1
         M = r*g*((1+X1*r*g*math.tanh(r)*H)**(-1))*math.tanh(r)*H
@@ -65,21 +75,24 @@ def calc(v, h):
         # print('\n\n\n')
         return r*f_fur*M
     
-    print("Численное решение интеграла по эпсилон\n", J)
-    J = round(J[0],3)
+    
     W = quad(integrate_W, 0, np.inf, args = (H) )
     a = p/(po*g)
-    print(a)
-    print(J)
-    print(W[0])
+    
+    # print("Численное решение интеграла по эпсилон\n", J)
+    J = round(J[0],3)
+    # print(a)
+    # print(J)
+    # print(W[0])
+    print(yj)
     print("Численное решение интеграла по r\n", W)
-    print("Значение прогиба, мм \n",(a*J*W[0])/(8*(3.1415**2)),"\n\n")
+    # print("Значение прогиба, мм \n",(a*J*W[0])/(8*(3.1415**2)),"\n\n")
     return (a*J*W[0])/(8*(3.1415**2))
 
 v = np.arange(0, 21, 0.5)
 w_t = []
 w_ex = []
-h = [3, 2.5, 2, 1.5]
+h = [1.5, 2, 2.5, 3]
 fig = plt.figure()
 ax1 = fig.add_subplot()
 for j in h:
--- Digital signal.py
+++ Digital signal.py
@@ -6,39 +6,71 @@ Created on Wed Sep 15 13:23:59 2021
 """
 import matplotlib.pyplot as plt
 import numpy as np
-
-V = np.array([0,0,5,5,5,5,0,0,0,0,5,5,5,0,0,0,5,5]) 
-T = np.array([0,1,1,2,3,4,4,5,6,7,7,8,9,9,10,11,11,12])
-if V[-1] != 0:
-    V = np.append(V,0)
-    T = np.append(T,T[-1])
-def Velosity(T, V, time_set = 0.1):
+import scipy as sp
+import scipy.signal
+import pandas as pd
+import math
+from tkinter.filedialog import askopenfilename
+def open_datafile(path,a=1,b=20000000):
+    """
+    Открыть файл с экспериментальными данными
+    Запрашивает: путь к файлу(строковый тип), левая граница промежутка, правая граница промежутка 
+    Возвращает массив из 3-х столбцов: время, сила, прогиб
+    """
     
-    RISE = [] # список точек "подъема" цифрового сигнала
-    FALL = [] # список точек "падения" цифрового сигнала
-    VEL = [] # список скоростей
-    VEL1 = [] # список скоростей
-    TIME = [] #список времени
+    try:
+        file= open(path,encoding= 'ansi')#перекодировка из utf-8 в ansi из за странной ошибки в спайдере
+        data=pd.read_csv(file,sep='\s+' ,decimal="." )
+        data=np.array(data.values)    #перевод значений в массив Numpy
+        float(data[-5,0])
+        float(data[-10,0])
+        
+    except ValueError:
+        file= open(path,encoding= 'ansi')#перекодировка из utf-8 в ansi из за странной ошибки в спайдере
+        data=pd.read_csv(file,sep='\s+' ,decimal="," ) #Читаю из тестового документа в качестве сепаратора: все пробелы
+        data=np.array(data.values) 
+        data = np.flip(data, axis = 1)#перевод значений в массив Numpy
+        
+    #у переменной data сейчас тип dataframe
+    file.close()
+    return data[a:(b+1)]
 
-    for i in np.where(V > 0)[0]: #цикл по всем точкам где напряжение больше 0
-        if V[i-1] < 5: 
-            RISE.append(i) # добавляем точку начала цифрового сигнала
-        if V[i+1] > 5:
-            FALL.append(i) # добавляем точку конца цифрового сигнала
-    for i in range(len(RISE)): 
-        if i!=len(RISE)-1:
-            VEL.append(1/(T[RISE[i+1]]-T[RISE[i]])) #1/dT где dТ время между двумя началами цифрового сигнала
-            VEL1.append(1/(T[FALL[i+1]]-T[FALL[i]])) 
-            TIME.append((T[RISE[i+1]]+T[RISE[i]])/2)# среднее время между двумя точками
-            
-    return np.column_stack((np.array(VEL),np.array(VEL1, np.array(TIME))))
+"""
+Переменные
+"""
+d = 3 # диаметр
+Volt_ch = 2 # граница напряжения относительно которой сравниваем
+med = 101 #размер ядра фильтрации медианного фильтра
 
+filename=askopenfilename()# Вызов окна открытия файла
+data=open_datafile(filename) #Открыть файл с данными 
+fig, ax = plt.subplots() # создаем объект figure и axes
+T = data[:,0]
+V = data[:,1]
 
-print(Velosity(T,V))
-fig, ax = plt.subplots()
+V = sp.signal.medfilt(V, med)
+
+V[0], V[-1] = 0, 0
+
+# поиск точек подъема и падения
+n = np.array(np.where(V > Volt_ch))[0,: ] # массив координат где V > 2
+n = np.vstack((n[np.where(V[n - 1] < Volt_ch)], n[np.where(V[n + 1] < Volt_ch)]))# двумерный массив с точками подъема и падения
+# первый столбец ищет точки на одну итерацию меньше где происходит изменение напряжения,
+# второй столбец аналогично но итерация вперед
+
+# Построение точек и графика напряжения от времени
 ax.plot(T, V)
+plt.scatter(T[n[0]], V[n[0]])#n[0] - индексы точек подъёма
+plt.scatter(T[n[1]], V[n[1]])#n[1] - индексы точек падения
+ax.set_xlabel('Время, сек')
+ax.set_ylabel('Напряжение, В \n Скорость, мм/сек')
+
+v = 1/(((T[n[0, 1:]] + T[n[1, 1:]]) / 2) - ((T[n[0, :-1]] + T[n[1, :-1]]) / 2)) 
+v = (math.pi* d /360) * np.hstack([ v, np.array(1 / (((T[n[0, -1]] + T[n[1, -1]]) / 2) - ((T[n[0, -2]] + T[n[1, -2]])/2)))])
+t = (((T[n[0, 1:]] + T[n[1, 1:]]) / 2) + ((T[n[0, :-1]] + T[n[1, :-1]]) / 2))/2
+t = np.hstack([ t, np.array((((T[n[0, -1]] + T[n[1, -1]]) / 2) + ((T[n[0, -2]] + T[n[1, -2]])/2)) / 2)])
+
+
+plt.plot(t,v)# скорость от времени построение графика
 plt.grid()
 plt.show()
-
-
-input()
